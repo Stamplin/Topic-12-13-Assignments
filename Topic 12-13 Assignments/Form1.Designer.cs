435655578818983c4b8d611d60b48bc4a7312c1d
Sakarona Bora <701017313@GoTVDSB.ca>
2024-10-30 09:39:14 -0400

    Add project files.

---
+++ Topic 12-13 Assignments/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Topic_12_13_Assignments
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radSmall = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radLarge = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkMayonnaise = new System.Windows.Forms.CheckBox();
+            this.chkOnion = new System.Windows.Forms.CheckBox();
+            this.chkTomato = new System.Windows.Forms.CheckBox();
+            this.chkCheese = new System.Windows.Forms.CheckBox();
+            this.chkMustard = new System.Windows.Forms.CheckBox();
+            this.chkLettuce = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lbleditCount = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radSmall
+            // 
+            this.radSmall.AutoSize = true;
+            this.radSmall.Location = new System.Drawing.Point(15, 19);
+            this.radSmall.Name = "radSmall";
+            this.radSmall.Size = new System.Drawing.Size(50, 17);
+            this.radSmall.TabIndex = 0;
+            this.radSmall.TabStop = true;
+            this.radSmall.Text = "Small";
+            this.radSmall.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gold;
+            this.groupBox1.Controls.Add(this.radLarge);
+            this.groupBox1.Controls.Add(this.radSmall);
+            this.groupBox1.Location = new System.Drawing.Point(12, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(420, 53);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sandwich Size;";
+            // 
+            // radLarge
+            // 
+            this.radLarge.AutoSize = true;
+            this.radLarge.Location = new System.Drawing.Point(135, 19);
+            this.radLarge.Name = "radLarge";
+            this.radLarge.Size = new System.Drawing.Size(52, 17);
+            this.radLarge.TabIndex = 1;
+            this.radLarge.TabStop = true;
+            this.radLarge.Text = "Large";
+            this.radLarge.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 2;
+            // 
+            // chkMayonnaise
+            // 
+            this.chkMayonnaise.AutoSize = true;
+            this.chkMayonnaise.Location = new System.Drawing.Point(114, 101);
+            this.chkMayonnaise.Name = "chkMayonnaise";
+            this.chkMayonnaise.Size = new System.Drawing.Size(83, 17);
+            this.chkMayonnaise.TabIndex = 3;
+            this.chkMayonnaise.Text = "Mayonnaise";
+            this.chkMayonnaise.UseVisualStyleBackColor = true;
+            // 
+            // chkOnion
+            // 
+            this.chkOnion.AutoSize = true;
+            this.chkOnion.Location = new System.Drawing.Point(6, 58);
+            this.chkOnion.Name = "chkOnion";
+            this.chkOnion.Size = new System.Drawing.Size(54, 17);
+            this.chkOnion.TabIndex = 4;
+            this.chkOnion.Text = "Onion";
+            this.chkOnion.UseVisualStyleBackColor = true;
+            // 
+            // chkTomato
+            // 
+            this.chkTomato.AutoSize = true;
+            this.chkTomato.Location = new System.Drawing.Point(6, 101);
+            this.chkTomato.Name = "chkTomato";
+            this.chkTomato.Size = new System.Drawing.Size(62, 17);
+            this.chkTomato.TabIndex = 5;
+            this.chkTomato.Text = "Tomato";
+            this.chkTomato.UseVisualStyleBackColor = true;
+            // 
+            // chkCheese
+            // 
+            this.chkCheese.AutoSize = true;
+            this.chkCheese.Location = new System.Drawing.Point(114, 19);
+            this.chkCheese.Name = "chkCheese";
+            this.chkCheese.Size = new System.Drawing.Size(62, 17);
+            this.chkCheese.TabIndex = 6;
+            this.chkCheese.Text = "Cheese";
+            this.chkCheese.UseVisualStyleBackColor = true;
+            // 
+            // chkMustard
+            // 
+            this.chkMustard.AutoSize = true;
+            this.chkMustard.Location = new System.Drawing.Point(114, 58);
+            this.chkMustard.Name = "chkMustard";
+            this.chkMustard.Size = new System.Drawing.Size(64, 17);
+            this.chkMustard.TabIndex = 7;
+            this.chkMustard.Text = "Mustard";
+            this.chkMustard.UseVisualStyleBackColor = true;
+            // 
+            // chkLettuce
+            // 
+            this.chkLettuce.AutoSize = true;
+            this.chkLettuce.Location = new System.Drawing.Point(6, 19);
+            this.chkLettuce.Name = "chkLettuce";
+            this.chkLettuce.Size = new System.Drawing.Size(62, 17);
+            this.chkLettuce.TabIndex = 8;
+            this.chkLettuce.Text = "Lettuce";
+            this.chkLettuce.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Gold;
+            this.groupBox2.Controls.Add(this.chkLettuce);
+            this.groupBox2.Controls.Add(this.chkMayonnaise);
+            this.groupBox2.Controls.Add(this.chkTomato);
+            this.groupBox2.Controls.Add(this.chkMustard);
+            this.groupBox2.Controls.Add(this.chkCheese);
+            this.groupBox2.Controls.Add(this.chkOnion);
+            this.groupBox2.Location = new System.Drawing.Point(12, 98);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(420, 144);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Fixings:";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.Gold;
+            this.btnCalculate.Location = new System.Drawing.Point(12, 261);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(419, 60);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Calculate Price";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.BackColor = System.Drawing.Color.Gold;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(15, 343);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(96, 25);
+            this.lblTotal.TabIndex = 11;
+            this.lblTotal.Text = "Total: 0$";
+            // 
+            // lbleditCount
+            // 
+            this.lbleditCount.AutoSize = true;
+            this.lbleditCount.BackColor = System.Drawing.Color.Yellow;
+            this.lbleditCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbleditCount.Location = new System.Drawing.Point(592, 9);
+            this.lbleditCount.Name = "lbleditCount";
+            this.lbleditCount.Size = new System.Drawing.Size(196, 13);
+            this.lbleditCount.TabIndex = 12;
+            this.lbleditCount.Text = "Order has been Changed: 0 times";
+            this.lbleditCount.Click += new System.EventHandler(this.lbleditCount_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::Topic_12_13_Assignments.Properties.Resources.fe0a0eee780652de948e6bf4b9ddf3e7;
+            this.ClientSize = new System.Drawing.Size(800, 463);
+            this.Controls.Add(this.lbleditCount);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "SubwayPOS";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radSmall;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radLarge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkMayonnaise;
+        private System.Windows.Forms.CheckBox chkOnion;
+        private System.Windows.Forms.CheckBox chkTomato;
+        private System.Windows.Forms.CheckBox chkCheese;
+        private System.Windows.Forms.CheckBox chkMustard;
+        private System.Windows.Forms.CheckBox chkLettuce;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lbleditCount;
+    }
+}
+
---
+++ Topic 12-13 Assignments/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_12_13_Assignments
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+        //submit btn here
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            double calCost, extraCost;
+            int editCount;
+            calCost = 0;
+            extraCost = 0;
+            //price for like the first size options
+            if (radSmall.Checked)
+            {
+                calCost = calCost + 2.50;
+            }
+            else if (radLarge.Checked)
+            {
+                calCost = calCost + 4;
+            }
+            
+            //secnd part
+            if (chkLettuce.Checked)
+            {
+                extraCost = extraCost + 0.10;
+            }
+            if (chkOnion.Checked)
+            {
+                extraCost = extraCost + 0.10;
+            }
+            if (chkTomato.Checked)
+            {
+                extraCost = extraCost + 0.10;
+            }
+            if (chkCheese.Checked) //expensive
+            {
+                extraCost = extraCost + 0.50;
+            }
+
+            //Final price and such things here
+            lblTotal.Text = "Total: " + (calCost + extraCost).ToString("C");
+            
+            editCount = ;
+            editCount++;
+            lbleditCount.Text = "Order has been Changed: " + (editCount) + " times";
+            
+            
+
+        }
+
+
+
+
+
+
+
+
+
+
+        private void lbleditCount_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
